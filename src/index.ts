a9a05fabbce18a795556c643b9526ce172fc5fd2
John Skilbeck <john.skilbeck@gmail.com>
2026-04-12 15:51:41 -0700

    fix: ts strict mode compat

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ export {
   type ReverseLogger,
 } from '#src/reverse-pino.js';
 
-export const infoLog = pino.create({ level: 'info' });
-export const debugLog = pino.create({ level: 'debug' });
-export const warnLog = pino.create({ level: 'warn' });
+export const infoLog: pino.Logger = pino.create({ level: 'info' });
+export const debugLog: pino.Logger = pino.create({ level: 'debug' });
+export const warnLog: pino.Logger = pino.create({ level: 'warn' });
 
-export const create = pino.create;
-export const mixin = pino.mixin;
+export const create: typeof pino.create = pino.create;
+export const mixin: typeof pino.mixin = pino.mixin;
 
 export type Logger = pino.Logger;
 export type LogLevels = pino.LogLevels;
